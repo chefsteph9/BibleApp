87fead136e214d20f46edefd1fe2fd8da5da0d16
Stephen Jenks <belizeanheat9@gmail.com>
2016-12-13 09:50:45 -0500

    update

---
+++ BibleApp/BibleApp/VerseDisplayPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BibleApp.Pages
+{
+    public class VerseDisplayPage : ContentPage
+    {
+        public VerseDisplayPage()
+        {
+
+            Label label = new Label
+            {
+                Text = verse.verse,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Selected Verse" },
+                    label
+                }
+            };
+        }
+    }
+}
